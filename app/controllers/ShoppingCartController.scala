b9baa09e419821ac60c067b4298e2bbe16d1bd8d
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-04-09 16:24:54 +0100

    initial add to cart model

--- app/controllers/ShoppingCartController.scala
+++ app/controllers/ShoppingCartController.scala
@@ -2,9 +2,13 @@ package controllers
 
 import db.DBUtil
 import javax.inject._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.db._
 import play.api.libs.json.Json
 import play.api.mvc._
+
+import scala.concurrent.Future
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -19,5 +23,40 @@ class ShoppingCartController @Inject()(
   def shoppingCart(): Action[AnyContent] = Action {
     val products = DBUtil.getShoppingCart(db, 1) // TODO: use customer id
     Ok(Json.toJson(products))
+  }
+
+//  fetch("http://localhost:9000/shopping-cart", {method: "post", headers: {
+//    "Content-Type": "application/json",
+//    //             "Content-Type": "application/x-www-form-urlencoded",
+//  }, body: JSON.stringify({product_id: 1, customer_id: 1})})
+
+  case class ShoppingItem(customer_id: Int, product_id: Int)
+  val userForm = Form(
+    mapping(
+      "customer_id" -> number,
+      "product_id" -> number
+    )(ShoppingItem.apply)(ShoppingItem.unapply)
+  )
+
+  def addToCart = Action(parse.form(userForm)) { implicit request =>
+    {
+//      userForm.bindFromRequest.fold(
+//        formWithErrors => {
+//          // binding failure, you retrieve the form containing errors:
+////          BadRequest(views.html.user(formWithErrors))
+//          Ok("hello")
+//        },
+//        shoppingItemData => {
+//          /* binding success, you get the actual value. */
+//          val item = ShoppingItem(shoppingItemData.customer_id,
+//                                  shoppingItemData.product_id)
+//          Ok("hello")
+//        }
+//      )
+//      val user = request.body.asFormUrlEncoded.get.head._1
+      val userData = request.body
+      val newUser = ShoppingItem(userData.customer_id, userData.product_id)
+      Ok("hello")
+    }
   }
 }
